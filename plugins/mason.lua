e7c5202ac6097e39bbd8fed1bd0a187e4c0a3ece
Vitalii Shtykhno <kilatib@gmail.com>
2024-01-10 07:29:57 +0100

    feat: lua settings

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,16 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "ansiblels",
+        "bashls",
+        "dockerls",
+        "docker_compose_language_service",
+        "jsonls",
+        "tsserver",
+        "marksman",
+        "intelephense",
+        "yamlls",
       })
     end,
   },
@@ -18,7 +27,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
+        "prettier",
         -- "stylua",
       })
     end,
@@ -30,6 +39,8 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "python",
+        "chrome",
+        "php"
       })
     end,
   },
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,59 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+   {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      local color_white = "#ebdbb2";
+      local color_gray = "#a1a1a1";
+      local color_black = "#282828";
+      local color_bg = "#232323";
+      local color_prompt = "#32302f";
+      local color_red = "#cc231d";
+      local color_yellow = "#d79921";
+      local color_green = "#b8bb26";
+      local color_orange = "#fe8019";
+
+      require("gruvbox").setup({
+        transparent_mode = true,
+        overrides = {
+          -- Status Line
+          NormalFloat = { fg = "#ebdbb2", bg = color_bg },
+          FloatBorder = { fg = color_bg, bg = color_bg },
+          StatusLine = { fg = "#ebdbb2", bg = color_bg },
+          -- Telescope General
+          TelescopeNormal = { bg = color_bg, fg = color_white },
+          TelescopeSelection = { bg = color_prompt, fg = color_white },
+          TelescopeSelectionCaret = { bg = "", fg = color_red },
+          TelescopeMultiSelection = { bg = "", fg = color_yellow },
+          TelescopeBorder = { bg = color_bg, fg = color_bg },
+          TelescopeResultsBorder = { bg = "", fg = "" },
+          TelescopePreviewBorder = { bg = "", fg = "" },
+          TelescopeMatching = { bg = "", fg = color_orange },
+          -- Prompt Section
+          TelescopePromptTitle = { bg = color_yellow, fg = color_black },
+          TelescopePromptPrefix = { bg = color_red, fg = color_red },
+          TelescopePrompt = { bg = color_orange, fg = "" },
+          TelescopePromptNormal = { bg = color_prompt, fg = color_white },
+          TelescopePromptBorder = { bg = color_prompt, fg = color_prompt },
+          -- Preview Section
+          TelescopePreviewTitle = { bg = color_green, fg = color_black },
+          -- CMP
+          CmpPmenu = { bg = color_bg },
+          CmpBorder = { fg = color_bg, bg = color_bg },
+          CmpDocBorder = { fg = color_bg, bg = color_bg },
+          CmpDoc = { bg = color_bg },
+          -- Diagnostics
+          DiagnosticFloatingError = { bg = color_bg, fg = color_red },
+          DiagnosticFloatingWarn = { bg = color_bg, fg = color_orange },
+          DiagnosticFloatingInfo = { bg = color_bg, fg = color_yellow },
+          DiagnosticFloatingHint = { bg = color_bg, fg = color_white },
+          -- Nerdtree and Nvim-Tree
+          NERDTreeHelp = { link = "NONE" },
+          -- WinBar
+          WinBarNC = { bg = color_black, fg = color_gray }
+        },
+      })
+    end,
+  },
 }
